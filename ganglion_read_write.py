4fd907077f80c718b33f1bb3782e12284ed697d2
Rishab Seshadri <rishab.seshadri@gmail.com>
2023-11-14 11:08:43 -0500

    Auto-search for device

--- ganglion_read_write.py
+++ ganglion_read_write.py
@@ -5,11 +5,12 @@ import threading
 import aioconsole
 import keyboard
 
-from bleak import BleakClient, BLEDevice
+from bleak import BleakClient, BleakScanner, BLEDevice
 
-address = "DA:23:8D:D7:85:97"
+address = ""
 read_uuid = "2d30c082-f39f-4ce6-923f-3484ea480596"
 write_uuid = "2d30c083-f39f-4ce6-923f-3484ea480596"
+SCALE = 0.001869917138805
 
 async def callback(sender, data):
     if (len(data) == 20 and data[19] != 0):
@@ -21,25 +22,45 @@ async def callback(sender, data):
         # TODO: fix noise filtering below
 
         extracted_bits = (full_int >> (num_bits - 27)) & mask
-        if extracted_bits * 0.001869917138805 < 800 or extracted_bits * 0.001869917138805 > 1000:
-            print(extracted_bits * 0.001869917138805)
+        extracted_bits *= SCALE
+        if extracted_bits < 800 or extracted_bits > 1000:
+            print(extracted_bits)
 
         extracted_bits = (full_int >> (num_bits - 103)) & mask # skip 84 + 19
-        if extracted_bits * 0.001869917138805 < 800 or extracted_bits * 0.001869917138805 > 1000:
-            print(extracted_bits * 0.001869917138805)
+        extracted_bits *= SCALE
+        if extracted_bits < 800 or extracted_bits > 1000:
+            print(extracted_bits)
 
 async def main(address):
+    
+    devices = await BleakScanner.discover()
+    for d in devices:
+        if d.name == "Ganglion-2b18" or d.name == "Simblee":
+            address = d.address
+            break
+
+    if address == "":
+        print("Ganglion not found.")
+        return
+    else:
+        print("Ganglion connected.")
+    
     async with BleakClient(address) as client:
         await client.start_notify(read_uuid, callback)
+    
+        print("Ganglion sending data...")
 
         await client.write_gatt_char(write_uuid,  bytes("b", "ascii")) 
+
+        print("Data stream started.")
 
         while True:
             input_str = await aioconsole.ainput("Press 'q' to quit: \n")
             if input_str == 'q':
-
+                print("Data stream ending...")
                 await client.write_gatt_char(write_uuid,  bytes("s", "ascii")) 
                 await client.disconnect()
+                print("Client disconnected.")
                 break
 
 
