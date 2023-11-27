3db1fabb7c0105d3492cfd53a93a7d05d855826d
Rishab Seshadri <rishab.seshadri@gmail.com>
2023-11-27 13:39:52 -0500

    Working frequency plot

--- ganglion_visualizer.py
+++ ganglion_visualizer.py
@@ -76,12 +76,12 @@ class ApplicationWindow(QMainWindow):
         self.main_widget = QWidget(self)
         layout = QVBoxLayout(self.main_widget)
         self.canvas1 = MplCanvas(self, width=5, height=4, dpi=100, plot_type="line")
-        layout.addWidget(self.canvas1)
+        # layout.addWidget(self.canvas1)
 
         self.canvas2 = MplCanvas(self, width=5, height=4, dpi=100, plot_type="point")
         layout.addWidget(self.canvas2)
-        self.canvas2.axes.set_ylim(-10000, 10000)
-        self.canvas2.axes.set_xlim(0, 100)
+        self.canvas2.axes.set_ylim(0, 10000)
+        self.canvas2.axes.set_xlim(0, 85)
 
         self.main_widget.setLayout(layout)
         self.setCentralWidget(self.main_widget)
@@ -177,12 +177,16 @@ write_uuid = "2d30c083-f39f-4ce6-923f-3484ea480596"
 SCALE = 0.001869917138805
 count = IntegerHolder(0)
 timer_thread = TimerThread()
-refresh_rate = 20
+refresh_rate = 100
+overlap = 50
 
 
 async def callback(sender, data):
     global nd_array_points
     global nd_array_times
+    global overlap
+
+    # Add new data to the arrays\
     if len(data) == 20 and data[19] != 0:
         full_int = int.from_bytes(data, "big")
 
@@ -193,47 +197,47 @@ async def callback(sender, data):
         extracted_bits *= SCALE
 
         if extracted_bits < 900 or extracted_bits > 1000:
-            if count.value % 5 == 0:
-                nd_array_points = np.insert(
-                    nd_array_points, nd_array_points.size - 1, [extracted_bits]
+            # if count.value % 5 == 0:
+            nd_array_points = np.insert(
+                nd_array_points, nd_array_points.size - 1, [extracted_bits]
+            )
+            nd_array_times = np.insert(
+                nd_array_times,
+                nd_array_times.size - 1,
+                [timer_thread.get_elapsed_time()],
+            )
+            if nd_array_points.size > refresh_rate:
+                """
+                filter.perform_lowpass(
+                    nd_array_points,  # data
+                    200,  # sampling rate
+                    150,  # cutoff frequency
+                    1,  # filter order
+                    FilterTypes.CHEBYSHEV_TYPE_1,  # filter type
+                    0,  # Chebyshev filter ripple value
                 )
-                nd_array_times = np.insert(
-                    nd_array_times,
-                    nd_array_times.size - 1,
-                    [timer_thread.get_elapsed_time()],
-                )
-                if nd_array_points.size > refresh_rate:
-                    """
-                    filter.perform_lowpass(
-                        nd_array_points,  # data
-                        200,  # sampling rate
-                        150,  # cutoff frequency
-                        1,  # filter order
-                        FilterTypes.CHEBYSHEV_TYPE_1,  # filter type
-                        0,  # Chebyshev filter ripple value
-                    )
-                    """
-                    fft_result = np.fft.fft(nd_array_points)
-                    frequencies = np.fft.fftfreq(len(nd_array_points), 1 / 200)
-                    magnitudes = np.abs(fft_result)
+                """
+                fft_result = np.fft.fft(nd_array_points)
+                frequencies = np.fft.fftfreq(len(nd_array_points), 1 / 200)
+                magnitudes = np.abs(fft_result)
 
-                    fft_result = np.fft.fft(nd_array_points)
+                fft_result = np.fft.fft(nd_array_points)
 
-                    half_index = len(frequencies) // 2
-                    positive_frequencies = frequencies[:half_index]
-                    positive_magnitudes = magnitudes[:half_index]
-                    sorted_indices = np.argsort(positive_frequencies)
-                    sorted_frequencies = positive_frequencies[sorted_indices]
-                    sorted_magnitudes = positive_magnitudes[sorted_indices]
+                half_index = len(frequencies) // 2
+                positive_frequencies = frequencies[:half_index]
+                positive_magnitudes = magnitudes[:half_index]
+                sorted_indices = np.argsort(positive_frequencies)
+                sorted_frequencies = positive_frequencies[sorted_indices]
+                sorted_magnitudes = positive_magnitudes[sorted_indices]
 
-                    # Now put the sorted data into the queue
-                    freq_queue.put((sorted_frequencies, sorted_magnitudes))
+                # Now put the sorted data into the queue
+                freq_queue.put((sorted_frequencies, sorted_magnitudes))
 
-                    for i in range(0, refresh_rate):
-                        data_queue.put((nd_array_times[i], nd_array_points[i]))
+                for i in range(overlap, refresh_rate - overlap):
+                    data_queue.put((nd_array_times[i], nd_array_points[i]))
 
-                    nd_array_points = np.array([0.0])
-                    nd_array_times = np.array([0.0])
+                nd_array_points = nd_array_points[overlap:]
+                nd_array_times = nd_array_times[overlap:]
             count.value += 1
         # if count.value % 5 == 0:
         # data_queue.put((timer_thread.get_elapsed_time(), extracted_bits))
@@ -241,48 +245,47 @@ async def callback(sender, data):
         extracted_bits = (full_int >> (num_bits - 103)) & mask  # skip 84 + 19
         extracted_bits *= SCALE
         if extracted_bits < 900 or extracted_bits > 1000:
-            if count.value % 5 == 0:
-                nd_array_points = np.insert(
-                    nd_array_points, nd_array_points.size - 1, [extracted_bits]
-                )
-                nd_array_times = np.insert(
-                    nd_array_times,
-                    nd_array_times.size - 1,
-                    [timer_thread.get_elapsed_time()],
-                )
-                if nd_array_points.size > refresh_rate:
-                    """
-                    filter.perform_lowpass(
-                        nd_array_points,  # data
-                        200,  # sampling rate
-                        150,  # cutoff frequency
-                        1,  # filter order
-                        FilterTypes.CHEBYSHEV_TYPE_1,  # filter type
-                        0,  # Chebyshev filter ripple value
-                    )"""
-                    fft_result = np.fft.fft(nd_array_points)
-                    frequencies = np.fft.fftfreq(len(nd_array_points), 1 / 200)
-                    magnitudes = np.abs(fft_result)
+            # if count.value % 5 == 0:
+            nd_array_points = np.insert(
+                nd_array_points, nd_array_points.size - 1, [extracted_bits]
+            )
+            nd_array_times = np.insert(
+                nd_array_times,
+                nd_array_times.size - 1,
+                [timer_thread.get_elapsed_time()],
+            )
+            if nd_array_points.size > refresh_rate:
+                """
+                filter.perform_lowpass(
+                    nd_array_points,  # data
+                    200,  # sampling rate
+                    150,  # cutoff frequency
+                    1,  # filter order
+                    FilterTypes.CHEBYSHEV_TYPE_1,  # filter type
+                    0,  # Chebyshev filter ripple value
+                )"""
+                fft_result = np.fft.fft(nd_array_points)
+                frequencies = np.fft.fftfreq(len(nd_array_points), 1 / 200)
+                magnitudes = np.abs(fft_result)
 
-                    fft_result = np.fft.fft(nd_array_points)
+                fft_result = np.fft.fft(nd_array_points)
 
-                    half_index = len(frequencies) // 2
-                    positive_frequencies = frequencies[:half_index]
-                    positive_magnitudes = magnitudes[:half_index]
-                    sorted_indices = np.argsort(positive_frequencies)
-                    sorted_frequencies = positive_frequencies[sorted_indices]
-                    sorted_magnitudes = positive_magnitudes[sorted_indices]
+                half_index = len(frequencies) // 2
+                positive_frequencies = frequencies[:half_index]
+                positive_magnitudes = magnitudes[:half_index]
+                sorted_indices = np.argsort(positive_frequencies)
+                sorted_frequencies = positive_frequencies[sorted_indices]
+                sorted_magnitudes = positive_magnitudes[sorted_indices]
 
-                    # Now put the sorted data into the queue
-                    freq_queue.put((sorted_frequencies, sorted_magnitudes))
+                # Now put the sorted data into the queue
+                freq_queue.put((sorted_frequencies, sorted_magnitudes))
 
-                    for i in range(0, refresh_rate):
-                        data_queue.put((nd_array_times[i], nd_array_points[i]))
+                for i in range(overlap, refresh_rate - overlap):
+                    data_queue.put((nd_array_times[i], nd_array_points[i]))
 
-                    nd_array_points = np.array([0.0])
-                    nd_array_times = np.array([0.0])
+                nd_array_points = nd_array_points[overlap:]
+                nd_array_times = nd_array_times[overlap:]
             count.value += 1
-        # if count.value % 5 == 0:
         # data_queue.put((timer_thread.get_elapsed_time(), extracted_bits))
 
 
