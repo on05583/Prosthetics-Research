81027ef08adcdf5d21caeb745119b58a8f2aff45
Rishab Seshadri <rishab.seshadri@gmail.com>
2023-12-01 17:17:08 -0500

    Prints when flexed

--- new_ganglion_visualizer.py
+++ new_ganglion_visualizer.py
@@ -5,24 +5,88 @@ You can run the following code using:
     
 
 """
-
 import asyncio
 import queue
 import threading
+import time
 
 import aioconsole
 import numpy as np
 from bokeh.models import ColumnDataSource, DataRange1d, Range1d
 from bokeh.plotting import curdoc, figure
+from brainflow.data_filter import *
+from paho.mqtt import client as mqtt_client
 
 from bleak import BleakClient, BleakScanner
 
 source = ColumnDataSource(dict(frequencies=[], magnitudes=[]))
+filter = DataFilter()
+input_value = 0
 
 p = figure(title="Live Data Stream", sizing_mode="stretch_both")
 p.x_range = Range1d(start=0, end=100)
-p.y_range = Range1d(start=0, end=10000)
+p.y_range = Range1d(start=0, end=1000)
 p.line(source=source, x="frequencies", y="magnitudes", line_width=2, alpha=0.85)
+
+
+def binary_search(arr, target):
+    if len(arr) <= 0:
+        return -1
+
+    low, high = 0, len(arr) - 1
+    closest_index = low
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if abs(arr[mid] - target) < abs(arr[closest_index] - target):
+            closest_index = mid
+
+        if arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            return mid
+
+    return closest_index
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+broker = "eduoracle.ugavel.com"
+port = 1883
+topic = "ganglion-data"
+client_id = f"python-mqtt-{681}"
+username = "giiuser"
+password = "giipassword"
+client = connect_mqtt()
+
+
+def publish(client, message):
+    msg_count = 1
+    while True:
+        time.sleep(0.001)
+        result = client.publish(topic, message)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{message}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
 
 
 def update_data(new_frequencies, new_magnitudes):
@@ -45,10 +109,12 @@ curdoc().add_periodic_callback(
 address = ""
 read_uuid = "2d30c082-f39f-4ce6-923f-3484ea480596"
 write_uuid = "2d30c083-f39f-4ce6-923f-3484ea480596"
-SCALE = 0.001869917138805
-num_fft_points = 200
+SCALE = 0.0001869917138805
+num_fft_points = 100
 freq_queue = queue.Queue()
 nd_array_points = np.array([0.0])
+start_index = 0
+stop_index = 0
 
 
 async def connect(address):
@@ -88,6 +154,8 @@ async def callback(sender, data):
     global nd_array_times
     global overlap
     global tick
+    global start_index
+    global stop_index
 
     # Add new data to the arrays
     if len(data) == 20 and data[19] != 0:
@@ -99,60 +167,123 @@ async def callback(sender, data):
         extracted_bits = (full_int >> (num_bits - 27)) & mask
         extracted_bits *= SCALE
 
-        if extracted_bits < 900 or extracted_bits > 1000:
+        if extracted_bits > 50 and (extracted_bits < 900 or extracted_bits > 1000):
             nd_array_points = np.insert(
                 nd_array_points, nd_array_points.size - 1, [extracted_bits]
             )
-
+            # 25 - 40
             # if tick > overlap:
             if len(nd_array_points) > num_fft_points:
+                filter.remove_environmental_noise(
+                    nd_array_points,  # data
+                    200,  # sampling rate
+                    NoiseTypes.FIFTY_AND_SIXTY,  # frequency
+                )
+                """
+                filter.perform_bandstop(
+                    nd_array_points,  # data
+                    200,  # sampling rate
+                    22,  # start freq
+                    25,  # stop freq
+                    1,  # order
+                    FilterTypes.BUTTERWORTH,
+                    0,
+                )
+                """
+
                 magnitudes = np.abs(np.fft.fft(nd_array_points))
                 frequencies = np.fft.fftfreq(len(nd_array_points), 1 / 200)
 
-                """
-                positive_frequencies = frequencies[:half_index]
-                positive_magnitudes = magnitudes[:half_index]
-                sorted_indices = np.argsort(positive_frequencies)
-                sorted_frequencies = positive_frequencies[sorted_indices]
-                sorted_magnitudes = positive_magnitudes[sorted_indices]
-
-                freq_queue.put((sorted_frequencies, sorted_magnitudes))
-                """
                 half_index = len(frequencies) // 2
 
                 # considers only positive half
                 freq_queue.put((frequencies[:half_index], magnitudes[:half_index]))
+
+                if len(frequencies) > 0:
+                    start_index = binary_search(frequencies, 15)
+                    stop_index = binary_search(frequencies, 40)
+
+                    sum_of_values = 0
+                    for i in range(start_index, stop_index):
+                        sum_of_values = magnitudes[i]
+
+                    input_value = sum_of_values / (stop_index - start_index)
+                    """
+                    if input_value > 1.8 and input_value <= 2.2:
+                        print("FLEX 1")
+                    elif input_value > 2.8:
+                        print("FLEX 2")
+                    else:
+                        print("...")
+                    """
+
+                    if input_value > 3:
+                        print("FLEX")
+                    else:
+                        print("...")
+
+                # publish(client, input_value)
 
                 nd_array_points = nd_array_points[1:]
 
         extracted_bits = (full_int >> (num_bits - 103)) & mask  # skip 84 + 19
         extracted_bits *= SCALE
 
-        if extracted_bits < 900 or extracted_bits > 1000:
+        if extracted_bits > 50 and (extracted_bits < 900 or extracted_bits > 1000):
             nd_array_points = np.insert(
                 nd_array_points, nd_array_points.size - 1, [extracted_bits]
             )
 
             # if tick > overlap:
             if len(nd_array_points) > num_fft_points:
+                filter.remove_environmental_noise(
+                    nd_array_points,  # data
+                    200,  # sampling rate
+                    NoiseTypes.FIFTY_AND_SIXTY,  # frequency
+                )
+                """
+                filter.perform_bandstop(
+                    nd_array_points,  # data
+                    200,  # sampling rate
+                    22,  # start freq
+                    25,  # stop freq
+                    1,  # order
+                    FilterTypes.BUTTERWORTH,
+                    0,
+                )
+                """
+
                 magnitudes = np.abs(np.fft.fft(nd_array_points))
                 frequencies = np.fft.fftfreq(len(nd_array_points), 1 / 200)
-
-                """
-                
-                positive_frequencies = frequencies[:half_index]
-                positive_magnitudes = magnitudes[:half_index]
-                sorted_indices = np.argsort(positive_frequencies)
-                sorted_frequencies = positive_frequencies[sorted_indices]
-                sorted_magnitudes = positive_magnitudes[sorted_indices]
-
-                freq_queue.put((sorted_frequencies, sorted_magnitudes))
-                """
 
                 half_index = len(frequencies) // 2
 
                 # considers only positive half
                 freq_queue.put((frequencies[:half_index], magnitudes[:half_index]))
+
+                if len(frequencies) > 0:
+                    start_index = binary_search(frequencies, 15)
+                    stop_index = binary_search(frequencies, 40)
+
+                    sum_of_values = 0
+                    for i in range(start_index, stop_index):
+                        sum_of_values = magnitudes[i]
+
+                    input_value = sum_of_values / (stop_index - start_index)
+
+                    """
+                    if input_value > 1.8 and input_value <= 2.2:
+                        print("FLEX 1")
+                    elif input_value > 2.8:
+                        print("FLEX 2")
+
+                    """
+
+                    if input_value > 3:
+                        print("FLEX")
+                    else:
+                        print("...")
+                # publish(client, input_value)
 
                 nd_array_points = nd_array_points[1:]
 
